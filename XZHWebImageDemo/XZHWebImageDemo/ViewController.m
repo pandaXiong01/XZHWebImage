42a40354475ec884b7e157a0663345cee2968dfd
熊志华 <gonghui@gonghuiOSdeMini.lan>
2016-06-08 17:54:08 +0800

    no message

--- XZHWebImageDemo/XZHWebImageDemo/ViewController.m
+++ XZHWebImageDemo/XZHWebImageDemo/ViewController.m
@@ -20,17 +20,12 @@
 @end
 
 @implementation ViewController
-- (NSMutableData *)data {
-    if (!_data) {
-        self.data = [NSMutableData data];
-    }
-    return _data;
-}
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 150, kScreenWidth, kScreenWidth)];
-    [imageView setImageWithURL:@"1" placeholderImageName:kImageUrl];
+//    [imageView setImageWithURL:@"1" placeholderImageName:kImageUrl];
     [self.view addSubview:imageView];
     
     UIButton *loadBtn = [UIButton buttonWithType:UIButtonTypeCustom];
@@ -61,15 +56,17 @@
         NSLog(@"线程:%@",[NSThread currentThread]);
         
         // 设置文件的存放目标路径
-//        NSString *documentsPath = [self getDocumentsPath];
-//        NSURL *documentsDirectoryURL = [NSURL fileURLWithPath:documentsPath];
-//        NSURL *fileURL = [documentsDirectoryURL URLByAppendingPathComponent:[[response URL] lastPathComponent]];
+        NSString *documentsPath = [self getDocumentsPath];
+        ///Users/gonghui/Library/Developer/CoreSimulator/Devices/80264F1F-2057-4B69-9ECC-BC89DA985CF1/data/Containers/Data/Application/164D1545-71B7-4DA9-BDBE-8BE440768B34/Documents
+        
+        NSURL *documentsDirectoryURL = [NSURL fileURLWithPath:documentsPath];
+        NSURL *fileURL = [documentsDirectoryURL URLByAppendingPathComponent:[[response URL] lastPathComponent]];
         // 如果该路径下文件已经存在，就要先将其移除，在移动文件
-//        NSFileManager *fileManager = [NSFileManager defaultManager];
-//        if ([fileManager fileExistsAtPath:[fileURL path] isDirectory:NULL]) {
-//            [fileManager removeItemAtURL:fileURL error:NULL];
-//        }
-//        [fileManager moveItemAtURL:location toURL:fileURL error:NULL];
+        NSFileManager *fileManager = [NSFileManager defaultManager];
+        if ([fileManager fileExistsAtPath:[fileURL path] isDirectory:NULL]) {
+            [fileManager removeItemAtURL:fileURL error:NULL];
+        }
+        [fileManager moveItemAtURL:location toURL:fileURL error:NULL];
         
         
         dispatch_async(dispatch_get_main_queue(), ^{
@@ -81,27 +78,6 @@
     [downloadTask resume];
     
 }
-/*
-#pragma mark -- NSURLSessionDataDelegate// 1.接收到服务器的响应
-- (void)URLSession:(NSURLSession *)session dataTask:(NSURLSessionDataTask *)dataTask didReceiveResponse:(NSURLResponse *)response completionHandler:(void (^)(NSURLSessionResponseDisposition))completionHandler {
-    //【注意：此处需要允许处理服务器的响应，才会继续加载服务器的数据。 若在接收响应时需要对返回的参数进行处理(如获取响应头信息等),那么这些处理应该放在这个允许操作的前面。】
-    completionHandler(NSURLSessionResponseAllow);
-}
-// 2.接收到服务器的数据（此方法在接收数据过程会多次调用）
-- (void)URLSession:(NSURLSession *)session dataTask:(NSURLSessionDataTask *)dataTask didReceiveData:(NSData *)data {
-    // 处理每次接收的数据，例如每次拼接到自己创建的数据receiveData
-    [self.data appendData:data];
-}
-// 3.3.任务完成时调用（如果成功，error == nil）
-- (void)URLSession:(NSURLSession *)session task:(NSURLSessionTask *)task didCompleteWithError:(NSError *)error {
-    if(error == nil){
-        self.imageView.image = [UIImage imageWithData:self.data];
-    }
-    else{
-        NSLog(@"请求失败:%@",error);
-    }
-}
-*/
 - (NSString *)getDocumentsPath {
     NSArray *documents = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
     NSString *documentsPath = documents[0];
